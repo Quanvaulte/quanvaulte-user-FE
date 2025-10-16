d1fbc2ae24d52a916f475d086948b804d845c3eb
9ightshade <aniekemeusanga@gmail.com>
2025-10-16 08:25:08 +0100

    chore:footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,30 @@
 export default function Footer() {
+  const footerItems = ["Home", "Email Marketing", "Courses", "FAQ"];
+
   return (
-    <div className="bg-blue-950">
-      <p>Are you ready?</p>
+    <div className="bg-[#040718] text-center  flex flex-col gap-6 p-20 ">
+      <p
+        className="font-normal
+      text-base
+      ">
+        Are you ready?
+      </p>
 
-      <h2>Let’s get started</h2>
+      <h2 className="text-4xl text-white  ">Let’s get started</h2>
 
-      <button>Get started</button>
+      <button
+        className="bg-[#F9D342]
+      rounded-lg text-[#2C43EB] text-lg font-bold py-3 px-8
+cursor-pointer  max-w-48 mx-auto mb-12   ">
+        Get started
+      </button>
 
-      <ul>
-        <li>Home</li>
-        <li>Email Marketing</li>
-        <li>Courses</li>
-        <li>FAQ</li>
+      <ul className="flex justify-center items-center gap-6 ">
+        {footerItems.map((item) => (
+          <li className="hover:text-[#F9D342] cursor-pointer" key={item}>
+            {item}
+          </li>
+        ))}
       </ul>
       <p>Copyright © 2020. Lo</p>
     </div>
