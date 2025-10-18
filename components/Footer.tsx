778d5909353c717456e76bce3741d14d57bb2da3
9ightshade <aniekemeusanga@gmail.com>
2025-10-18 23:21:03 +0100

    footer button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,21 +3,32 @@ export default function Footer() {
 
   return (
     <div className="bg-[#040718] text-center max-h-[511px] flex flex-col gap-6 py-24 ">
-      <p
+      {/* <p
         className="font-normal
       text-base
       ">
         Are you ready?
-      </p>
+      </p> */}
 
-      <h2 className="text-4xl text-white  ">Let’s get started</h2>
+      <h2 className="text-4xl text-white font-normal font-baloo ">
+        Let the learning begin with
+        <span className="text-[#F9D342] font-semibold"> Quanvaulte </span>today
+      </h2>
 
-      <button
-        className="bg-[#F9D342]
-      rounded-lg text-[#2C43EB] text-lg font-bold py-3 px-12
-cursor-pointer  max-w-48 mx-auto mb-12   ">
-        Get started
-      </button>
+      <div className="flex items-center gap-2 justify-center">
+        <button
+          className="
+      rounded-[40px] border-3 border-white shadow-xl text-white text-lg font-bold py-3 px-12
+cursor-pointer   ">
+          Login
+        </button>
+        <button
+          className="bg-white
+      rounded-[40px] text-[#2C43EB] text-lg font-bold py-3 px-12
+cursor-pointer    ">
+          Get started
+        </button>
+      </div>
 
       <ul className="flex justify-center items-center max-w-[493px] mx-auto gap-10 ">
         {footerItems.map((item) => (
