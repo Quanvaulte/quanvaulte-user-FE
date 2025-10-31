8fca812b4a4d30c72aecddb71bf34dae5e67915e
9ightshade <aniekemeusanga@gmail.com>
2025-10-31 01:41:29 +0100

    update text-color

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 export default function DashboardPage() {
   return (
     <div className="min-h-screen w-full flex bg-gray-50">
-      <h1 className="m-auto text-3xl font-bold">Welcome to your Dashboard</h1>
+      <h1 className="m-auto text-3xl text-black font-bold">
+        Welcome to your Dashboard
+      </h1>
     </div>
   );
 }
